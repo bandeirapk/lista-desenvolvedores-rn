4bf77e7c9a9e8dc697fa8f9078ab2f66732553ff
Antonio Bandeira <bandeiramagalhaesoficial@gmail.com>
2022-11-17 16:29:09 -0300

    Usando o styleSheet

--- App.tsx
+++ App.tsx
@@ -1,26 +1,33 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-    }}>
-      <Text style={{
-        color: '#fdfcfe',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Nome do evento
       </Text>
 
-      <Text style={{
-        color: '#6b6b6b',
-        fontSize: 16
-      }}>
+      <Text style={styles.eventDate}>
         Quinta, 17 de Novembro de 2022.
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16
+  }
+});
